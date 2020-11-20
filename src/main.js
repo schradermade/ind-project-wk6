3357eae02801d7a2f5d4f7937bf21badef5b1ea3
Nathan Schrader <nathanschrader@icloud.com>
2020-11-20 12:05:42 -0800

    add clearFields function, document.ready

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import Object from './js/object.js';
-// import Post from './js/post.js';
+import ExchangeRate from './js/exchange-service.js';
+
+function clearFields()  {
+  $('#amount').val("");
+  $('#currency-code').val("");
+  $('#show-exchange').val("");
+}
+
+function getExchange(response)  {
+}
+
+
+$(document).ready(function()  {
+  $('form-one').click(function()  {
+    let code = $('#currency-code').val();
+    clearFields();
+    ExchangeRate.getRate(code)
+      .then(function(response)  {
+        getExchange(response);
+      });
+  });
+});
