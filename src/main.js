412a581f0b77757459f568143498479f4bbb0059
Nathan Schrader <nathanschrader@icloud.com>
2020-11-24 18:41:48 -0800

    add full functionality to getExchange function

--- src/main.js
+++ src/main.js
@@ -10,38 +10,49 @@ function clearFields()  {
   $('#show-exchange').val("");
 }
 
-function convertCode(code)  {
-  let convCode = `response.conversion_chart.${code}`;
-  return convCode;
-}
-
-function getExchange(response, convCode, amount)  {
-  if (response.conversion_rates)  {
-    console.log(`this will ${convCode}`);
-    //let currCode = `response.conversion_rates.${code}`;
-    let code1 = response.conversion_rates;
-    console.log(code1);
-    //console.log(response.`${conversion_rates} ${code}`);
-    //console.log(response.conversion_rates.CAD);
-    //let convertAmount = currCode;
-    //console.log(response.conversion_rates.CAD);
-    $("#show-exchange").text(`Your $${amount} U.S. Dollars converts to  `);
-  } else  {
+function getExchange(response, code, amount)  {
+  let outputString;
+  let exchangedAmount;
+  const date = new Date();
+  if (response.conversion_rates) {
+    if (code === "CHF") {
+      exchangedAmount = Math.round(100 * (response.conversion_rates.CHF * amount))/100;
+      outputString = `Your ${amount} U.S. Dollars is ${exchangedAmount} Swiss Franc (CHF) with an exchange rate of ${response.conversion_rates.CHF} as of ${date}.`;
+    }
+    if (code === "CAD") {
+      exchangedAmount = Math.round(100 * (response.conversion_rates.CAD * amount))/100;
+      outputString = `Your ${amount} U.S. Dollars is ${exchangedAmount} Canadian Dollars (CAD) with an exchange rate of ${response.conversion_rates.CAD} as of ${date}.`;
+    }
+    if (code === "MXN") {
+      exchangedAmount = Math.round(100 * (response.conversion_rates.MXN * amount))/100;
+      outputString = `Your ${amount} U.S. Dollars is ${exchangedAmount} Mexican peso (MXN) with an exchange rate of ${response.conversion_rates.MXN} as of ${date}.`;  
+    }
+    if (code === "CNY") {
+      exchangedAmount = Math.round(100 * (response.conversion_rates.CNY * amount))/100;
+      outputString = `Your ${amount} U.S. Dollars is ${exchangedAmount} Chinese yuan (CNY) with an exchange rate of ${response.conversion_rates.CNY} as of ${date}.`;
+    }
+    if (code === "EUR") {
+      exchangedAmount = response.conversion_rates.EUR * amount;
+      outputString = `Your ${amount} U.S. Dollars is ${exchangedAmount} Euro (EUR) with an exchange rate of ${response.conversion_rates.EUR} as of ${date}.`;
+    } 
+    document.getElementById("showString").innerHTML = outputString;
+  } 
+  else  {
     $('#show-errors').text(`There was an error: ${response.message}`);
   }
 }
 
+
+
 $(document).ready(function()  {
   $('#formOne').submit(function(event)  {
     event.preventDefault();
-    let USD = "USD";
     let code = $('#code').val();
     let amount = $('#amount').val();
     clearFields();
-    ExchangeRate.getRate(USD)
+    ExchangeRate.getRate()
       .then(function(response)  {
         getExchange(response, code, amount);
-        convertCode(code);
       });
   });
 });
